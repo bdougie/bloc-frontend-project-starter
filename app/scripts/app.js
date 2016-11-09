7d074a666c3aa2e0b0e3a83ce603573c42e32959
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-11-08 23:15:46 -0800

    checkpoint 1

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("blocChat", ["firebase"]);
+
+app.controller("ChatCtrl", function($scope, $firebaseObject) {
+  var ref = firebase.database().ref();
+  // download the data into a local object
+  $scope.data = $firebaseObject(ref);
+  // putting a console.log here won't work, see below
+});
+
